8c6da25439deae45aac392ec0b0957d98f43c69d
Hrydya <143895876+Hrydya@users.noreply.github.com>
2026-03-23 00:20:36 +0530

    add habit model

---
+++ server/models/habitmodel.js
@@ -0,0 +1,24 @@
+const mongoose= require('mongoose')
+const habitschema= new mongoose.Schema({
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true,
+        },
+        name:{
+            type:String,
+            required: [true, "must provide a name"],
+            trim: true,
+            maxlength: [20, "name can't be more than 20 characters"]
+        },
+        completed:{
+            type:Boolean,
+            default:false
+        },
+        streak:{
+            type:Number,
+            default:0
+        }
+
+})
+module.exports= mongoose.model('Habit',habitschema)
